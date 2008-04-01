60fef717a52a4551e995ba6aaa4f67e24d276bae
ruckdeschel <ruckdeschel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-04-01 11:22:54 +0000

    minor "cosmetic" change in solve
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/distr@112 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/distrMod/R/solve.R
+++ pkg/distrMod/R/solve.R
@@ -2,16 +2,16 @@ setMethod("solve", signature(a = "ANY", b = "ANY"), function(a,b, generalized = 
           tol = .Machine$double.eps, ...) {if(!generalized) return(base::solve(a,b, tol = tol, ...))
           else if(is(try(return(base::solve(a,b, tol = tol, ...)), silent = TRUE),
                                         "try-error")){
+             if (!missing(b))
+                if(!(length(b)==nrow(a))) stop("non-conformable arguments")
              a.svd <- svd(a)
              d1 <- a.svd$d
              d1.0 <- (d1 < tol) + 0.0
              d1.1 <- 1/pmax(d1, d1.0)
              d <- (1-d1.0) * d1.1
-             d <- if (length(d)==1) d else diag(d)
+             d <- if (length(d) == 1) d else diag(d)
              a.m <- a.svd$v %*% d %*% t(a.svd$u)
-             if (!missing(b))
-                if(!(length(b)==nrow(a))) stop("non-conformable arguments")
-             erg <- if (missing(b)) a.m else a.m%*%b
+             erg <- if (missing(b)) a.m else a.m %*% b
              return(erg)}
              }
              )
@@ -24,7 +24,7 @@ setMethod("solve", signature(a = "PosSemDefSymmMatrix", b = "ANY"), function(a,b
             d1.0 <- (d1 < tol) + 0.0
             d1.1 <- 1/pmax(d1, d1.0)
             d <- (1-d1.0) * d1.1
-            d <- if (length(d)==1) d else diag(d)
+            d <- if (length(d) == 1) d else diag(d)
             A <- er$vectors %*% d %*% t(er$vectors)
             if(missing(b)) return(A)
             else return(A%*%b)}
