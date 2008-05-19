00d0ecb69287b3a5fbca752ebd3dd8383f4e08ff
ruckdeschel <ruckdeschel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-05-19 11:37:18 +0000

    deleted subfolder branches
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/distr@154 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/distr-2.0/branches/pkg/distrEx/R/UnivariateCondDistribution.R
+++
@@ -1,9 +0,0 @@
-## access methods
-setMethod("name", "Condition", function(object) object@name)
-
-## replace methods
-setReplaceMethod("name", "Condition", 
-    function(object, value){ object@name <- value; object})
-
-## access methods
-setMethod("cond", "UnivariateCondDistribution", function(object) object@cond)
--- branches/distr-2.0/branches/pkg/distrTeach/R/AllGeneric.R
+++
@@ -1,9 +0,0 @@
-############################################################################
-# generics to  "usual"  methods
-############################################################################
-
-if(!isGeneric("plotCLT")){
-  setGeneric("plotCLT", function(Tn, ...) standardGeneric("plotCLT"))
-}
-
-
