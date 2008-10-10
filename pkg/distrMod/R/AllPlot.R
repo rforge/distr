b573644f0b03d981cf8f6cb095ad60e3da1da45f
ruckdeschel <ruckdeschel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-10-10 23:18:47 +0000

    small changes in AllPlot.R in distrMod
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/distr@305 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/distrMod/R/AllPlot.R
+++ pkg/distrMod/R/AllPlot.R
@@ -12,6 +12,13 @@ setMethod("plot", "L2ParamFamily",
         dots <- match.call(call = sys.call(sys.parent(1)), 
                        expand.dots = FALSE)$"..."
 
+        if(!is.null(dots[["lty"]]))  dots["lty"] <- NULL
+        if(!is.null(dots[["type"]])) dots["type"] <- NULL
+        if(!is.null(dots[["main"]])) dots["main"] <- NULL
+        if(!is.null(dots[["sub"]]))  dots["sub"] <- NULL
+        if(!is.null(dots[["xlab"]])) dots["xlab"] <- NULL
+        if(!is.null(dots[["ylab"]])) dots["ylab"] <- NULL
+
         e1 <- x@distribution
         if(!is(e1, "UnivariateDistribution")) stop("not yet implemented")
 
@@ -36,11 +43,6 @@ setMethod("plot", "L2ParamFamily",
                 lty <- "dotted"
             }
         }
-
-        dots["lty"]  <- NULL
-        dots["type"] <- NULL
-        dots["xlab"] <- NULL
-        dots["ylab"] <- NULL
         
         dims <- length(x@param)
         L2deriv <- as(diag(dims) %*% x@L2deriv, "EuclRandVariable")
@@ -53,7 +55,7 @@ setMethod("plot", "L2ParamFamily",
         ncols <- ceiling(dims/nrows)
         par(mfrow = c(nrows, ncols))
 
-        if(is.null(dots[["cex.main"]])) dots[["cex.main"]] <- 0.8
+        if(is.null(dots[["cex.main"]])) dots["cex.main"] <- 0.8
                 
         for(i in 1:dims){
             do.call(plot, args=c(list(x=x.vec, y=sapply(x.vec, L2deriv@Map[[i]]), 
@@ -63,7 +65,8 @@ setMethod("plot", "L2ParamFamily",
                                  dots))
             if(is(e1, "DiscreteDistribution")){
                 x.vec1 <- seq(from = min(x.vec), to = max(x.vec), length = 1000)
-                lines(x.vec1, sapply(x.vec1, L2deriv@Map[[i]]), lty = "dotted")
+                do.call(lines, args=c(list(x.vec1, sapply(x.vec1, L2deriv@Map[[i]]), 
+                              lty = "dotted"),dots))
             }
             if(is.null(x@param@nuisance))
                 do.call(title, args = c(list(paste("Component", i, "of L_2 derivative\nof", name(x)[1], 
