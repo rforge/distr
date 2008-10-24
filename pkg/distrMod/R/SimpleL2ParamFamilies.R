5ff3cc6b230c9fd41d0f545bb805388e76a7a944
stamats <stamats@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-10-24 08:26:52 +0000

    in my opinion definition of LnormScaleFamily was wrong ... corrected ...
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/distr@315 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/distrMod/R/SimpleL2ParamFamilies.R
+++ pkg/distrMod/R/SimpleL2ParamFamilies.R
@@ -362,11 +362,18 @@ LnormScaleFamily <- function(meanlog = 0, sdlog = 1, trafo){
                   centraldistribution = Lnorm(meanlog = 0, sdlog = sdlog),
                   modParam = modParam,
                   LogDeriv = function(x) log(x)/x/sdlog^2 + 1/x,
-                  L2derivDistr.0 = AbscontDistribution(r=function(n){
-                    x <- rlnorm(n); (log(x)-1)/x}),
-       ###wrong!!!           L2derivDistr.0 = Norm(mean=0, sd=1/sdlog^2),
-       ###wrong!!!           FisherInfo.0 = matrix(1/sdlog^2,
-       ###wrong!!!                                 dimnames = list("scale","scale")), 
+#                  L2derivDistr.0 = AbscontDistribution(r=function(n){
+#                    x <- rlnorm(n); (log(x)-1)/x}),
+# wrong in my opinion
+# (x/scale*LogDeriv(x/scale) - 1)/scale = (log(x/scale)/sdlog^2 + 1 - 1)/scale
+#                                       = log(x/scale)/sdlog^2/scale
+#                                       = (log(x) - meanlog)/sdlog/(sdlog*scale)
+# now x ~ Lnorm(meanlog, sdlog)
+# => log(x) ~ Norm(meanlog, sdlog^2)
+# => (log(x) - meanlog)/sdlog ~ Norm(0, 1)
+                  L2derivDistr.0 = Norm(mean=0, sd=1/sdlog/exp(meanlog)),
+                  FisherInfo.0 = matrix(1/exp(2*meanlog)/sdlog^2,
+                                        dimnames = list("scale","scale")), 
                   distrSymm = NoSymmetry(), 
                   L2derivSymm = FunSymmList(NonSymmetric()), 
                   L2derivDistrSymm = DistrSymmList(SphericalSymmetry(SymmCenter = 0)),
