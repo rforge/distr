75dbafcea12b2338ea4ef226c3e6453475721f04
stamats <stamats@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-10-30 10:52:26 +0000

    forgot to remove coercing ...
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/distr@323 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/distr/R/DiscreteDistribution.R
+++ pkg/distr/R/DiscreteDistribution.R
@@ -197,7 +197,7 @@ function(e1,e2){
             e1 <- as(e1, "LatticeDistribution")
             e2 <- as(e2, "LatticeDistribution")
             if(is(e1, "LatticeDistribution") & is(e2, "LatticeDistribution"))
-                return(as(e1 + e2, "DiscreteDistribution"))
+                return(e1 + e2)
             convolutedsupport <- rep(support(e1), each = length(support(e2))) +
                                  support(e2)
 
