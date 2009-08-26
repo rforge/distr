9f99792e685e9b29b05e1dc7d46661e14e7f7f57
ruckdeschel <ruckdeschel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-08-26 00:56:12 +0000

    +distrMod: yet another issue with "pretty printing", last commit did not help; instead give default names "" if empty
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/distr@552 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/distr-2.2/pkg/distrMod/R/0distrModUtils.R
+++ branches/distr-2.2/pkg/distrMod/R/0distrModUtils.R
@@ -34,20 +34,21 @@
 }
 
 .show.with.sd <- function(est, s){
-        est <- as.numeric(est); dim(est) <- NULL
-        s <- as.numeric(s); dim(s) <- NULL
+#        est <- as.numeric(est); dim(est) <- NULL
+#        s <- as.numeric(s); dim(s) <- NULL
+        if(is.null(names(est))) names(est) <- rep("", length.out=length(est))
   ### code borrowed from print.fitdistr in  package MASS by B.D. Ripley
         digits <- getOption("digits")
         ans <- format(base::rbind(est, s), digits=digits)
-        ans[1, ] <- sapply(ans[1, ], function(x) paste("", x))
-        ans[2, ] <- sapply(ans[2, ], function(x) paste("(", x, ")", sep=""))
+        ans[1L, ] <- sapply(ans[1L, ], function(x) paste("", x))
+        ans[2L, ] <- sapply(ans[2L, ], function(x) paste("(", x, ")", sep=""))
      ## only used for digits
         dn <- dimnames(ans)
-        dn[[1]] <- rep("", 2)
-        dn[[2]] <- paste(substring("      ", 1, 
-                       (nchar(ans[2,]) - nchar(dn[[2]])) %/% 2), dn[[2]])
-        dn[[2]] <- paste(dn[[2]], substring("      ", 1, 
-                       (nchar(ans[2,]) - nchar(dn[[2]])) %/% 2))
+        dn[[1L]] <- rep("", 2L)
+        dn[[2L]] <- paste(substring("      ", 1L,
+                       (nchar(ans[2L,]) - nchar(dn[[2L]])) %/% 2), dn[[2L]])
+        dn[[2L]] <- paste(dn[[2L]], substring("      ", 1L,
+                       (nchar(ans[2L,]) - nchar(dn[[2L]])) %/% 2))
         dimnames(ans) <- dn
         print(ans, quote = FALSE)
         return(invisible())
