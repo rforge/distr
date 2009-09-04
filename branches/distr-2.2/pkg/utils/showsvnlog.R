0f0e1c962d2fe403b97c07181f6d414e3cc4a04c
ruckdeschel <ruckdeschel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-09-04 15:07:53 +0000

    all packages:   updated NEWS files   inserted remark section on package versions into each <pkg>-package.Rd file distrEllipse: had forgotten some .Rd files utils: has a NEWS and a TOBEDONE file now; reorganized some files
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/distr@575 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/distr-2.2/pkg/utils/showsvnlog.R
+++ branches/distr-2.2/pkg/utils/showsvnlog.R
@@ -1,48 +1,5 @@
-### utility to concatenate all files of same kind in one output
-showAllFiles <- function(
-         type = "TOBEDONE$", ### type of Files to be concatenated
-                             ### typically something like
-                             ### DESCRIPTION, NEWS, LICENSE, TOBEDONE, CITATION
-         dir = "C:/rtest/distr/", ### top folder from which to recurse
-         except = NULL,      ### some exceptions (regular expressions!)
-         con = stdout()      ### where to write the results to
-                         ){
-  myline <- paste(rep("-",length.out=78),collapse="",sep="")
-  oldDir <- getwd()
-  on.exit(setwd(oldDir))
-  printToBeDone <- function(dir0){
-    setwd(dir0)
-    cat("\n")
-    printout <- function(y){
-       #y
-       zz <- readLines(y, warn = FALSE)
-       writeLines(myline, con=con)
-       writeLines(paste("File: ",y,collapse=""), con=con)
-       writeLines(myline, con=con)
-       writeLines(zz, con=con)
-       writeLines("\n", con = con)
-       invisible()
-       }
-    DIR <- grep(type, dir(".", rec = TRUE), value=TRUE)
-    if(!is.null(except))
-       DIR <- DIR[-grep(except,DIR)]
-    s <- lapply(DIR,printout)
-    invisible()
-    }
-  printToBeDone(dir)
-  invisible()
-}
-
-if(FALSE){
- ### some examples
-showAllFiles()
-showAllFiles(Type="NEWS", except="www")
-showAllFiles(dir="C:/rtest/robast")
-
-}
-
-if(FALSE){
 ### does not really work / Tortoise is to be used interactively...
+if(FALSE){
 showsvnlogWithTortoise <- function(pathRepo = "distr",
     inRforge = TRUE,
     PathToTortoise="C:/Program Files/TortoiseSVN",
@@ -106,4 +63,6 @@ if(FALSE){
 ### some examples
 showsvnlog(from=500,to=520)
 showsvnlog(from=300,to=Inf,limit=1000)
+showsvnlog(from=1,to=Inf,limit=1000,con="C:/rtest/svnlog-distr.txt")
+showsvnlog("robast",from=1,to=Inf,limit=1000,con="C:/rtest/svnlog-robast.txt")
 }
