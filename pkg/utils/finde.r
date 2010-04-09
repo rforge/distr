c6f44d95019b1c3afa1018c2dc971c0a5cd70670
ruckdeschel <ruckdeschel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-04-09 14:23:54 +0000

    triggered by mail by Sven Sewitz sas69@cam.ac.uk, replaced all dead links to distr.pdf in www files by url to vignette of distrDoc on CRAN; also replaced uni-bayreuth.de mail address by itwm.fraunhofer.de one
    to do this used new util ersetze (similar to finde) now in folder utils
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/distr@644 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/utils/finde.r
+++ pkg/utils/finde.r
@@ -20,6 +20,33 @@ finde <- function(x = "nchar", dir="C:/rtest/distr/pkg/distr/R", ext = "R", rec 
   }
 infind(dir)   
 }
-finde(x=".makeQNew", dir ="C:/rtest/distr/pkg/distr/R")
-finde(x=".makeQNew", dir ="C:/rtest/distr/pkg/distr", rec=TRUE)
 
+
+finde(x=".makeQNew", dir ="C:/rtest/distr/pkg/distr/R")
+finde(x="http://distr\\.r-forge\\.r-project\\.org/distr\\.pdf", dir ="C:/rtest/distr/", rec=TRUE)
+
+ersetze <- function(x0 = "nchar", x1="nchar", dir="C:/rtest/distr/pkg/distr/R", ext = "R", rec = FALSE){
+  ow <- getwd()
+  on.exit(setwd(ow))
+  infind <- function(dir0){
+    setwd(dir0)
+    cat("\n")
+    findL <- function(y){
+       zz <- readLines(y)
+       lgr <- grep(x0,zz)
+       writeLines(gsub(x0,x1,zz),y)
+       if(length(lgr>0)){
+          if(rec)
+             cat(paste(dir0,"/",y,sep="")," :: ",paste(lgr),"\n")
+          else cat(y," :: ",paste(lgr),"\n")
+       }
+       invisible()
+    }
+  ext0 <- if(ext=="") "" else paste("\\.", ext, sep="")
+  DIR <- grep(ext0, dir(, rec = rec), value=TRUE)
+  s <- lapply(DIR,findL)
+  }
+infind(dir)
+}
+ersetze(x0="href=\"distr.pdf\"",x1="href=\"http://cran.r-project.org/web/packages/distrDoc/vignettes/distr.pdf\"", dir ="C:/rtest/distr/www", rec=TRUE,ext="html")
+ersetze(x0="peter.ruckdeschel@uni-bayreuth.de",x1="peter.ruckdeschel@itwm.fraunhofer.de", dir ="C:/rtest/distr/www", rec=TRUE, ext="html")
