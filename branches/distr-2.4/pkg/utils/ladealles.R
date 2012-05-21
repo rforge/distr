a41a4a879f93fd06fd80bd7e5b6283e36e978d47
ruckdeschel <ruckdeschel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-05-21 00:01:24 +0000

    [utils] in helper utility ladealles: print out can be suppressed
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/distr@815 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/distr-2.4/pkg/utils/ladealles.R
+++ branches/distr-2.4/pkg/utils/ladealles.R
@@ -1,12 +1,15 @@
- ladeall <- function(DIR="distr", develDir = "C:/rtest/distr/pkg",pattern=""){
+ ladeall <- function(DIR="distr", develDir = "C:/rtest/distr/pkg",pattern="",
+          withPrint = TRUE){
  od <- getwd()
- print(file.path(develDir,DIR, "R"))
+ if(withPrint) print(file.path(develDir,DIR, "R"))
  setwd(file.path(develDir,DIR, "R"))
- lapply(grep(paste(pattern,".(r|R)$",sep="",collapse=""),dir(),value=T),function(...) {print(...);source(...)})
+ lapply(grep(paste(pattern,".(r|R)$",sep="",collapse=""),dir(),value=T),
+          function(...) {if(withPrint) print(...);source(...)})
  setwd(od)
 }
 
 #ladeall(DIR="distr", develDir = "C:/rtest/distr/branches/distr-2.1/pkg")
 #ladeall(DIR="distrEx", develDir = "C:/rtest/distr/pkg")
-ladeall(DIR="ROptEst", develDir = "C:/rtest/robast/branches/robast-0.7/pkg")
+ladeall(DIR="distrMod", develDir = "C:/rtest/distr/branches/distr-2.4/pkg",withPrint=FALSE)
+ladeall(DIR="RobExtremes", develDir = "C:/rtest/robast/branches/robast-0.9/pkg")
 
