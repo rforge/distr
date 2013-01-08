783726b9cecad2318fd7755f78300295f10e0a16
ruckdeschel <ruckdeschel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-01-08 21:06:35 +0000

    distrEx: collected functions moved to RobExtremes to issue notification
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/distr@832 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ branches/distr-2.4/pkg/distrEx/R/moved2RobExtremes.R
@@ -0,0 +1,13 @@
+################ commands that have moved to RobExtremes
+
+.mv2RobExtremes <- function(){
+  cat("\n\n This functionality has moved to package RobExtremes.\n\n")
+}
+
+kMAD <- function(x,k) .mv2RobExtremes()
+Pareto <- function(shape, Min) .mv2RobExtremes()
+Pareto <- function(shape, Min) .mv2RobExtremes()
+GPareto <- function(loc, scale, shape, location) .mv2RobExtremes()
+GEV <- function(loc, scale, shape, location) .mv2RobExtremes()
+EULERMASCHERONICONSTANT  <- .mv2RobExtremes()
+APERYCONSTANT <- .mv2RobExtremes()
