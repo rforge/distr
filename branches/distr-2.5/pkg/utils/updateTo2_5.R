1dafe1d54bb9ca98e6be82702b91c6ec6add3f01
ruckdeschel <ruckdeschel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-01-09 01:01:41 +0000

    minor changes for update to 2.5
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/distr@839 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/distr-2.5/pkg/utils/updateTo2_5.R
+++ branches/distr-2.5/pkg/utils/updateTo2_5.R
@@ -3,24 +3,24 @@
 Pkgs <- c("SweaveListingUtils", "distr", "distrEx", "distrDoc",
                       "distrMod", "distrTeach", "distrSim", "distrTEst",
                       "distrEllipse")
-Names <- c("Version","Date","SVNRevision")
-Values <- matrix(c("2.0.2", format(Sys.time(), format="%Y-%m-%d"),1),3,length(Pkgs))
-colnames(Values) <- Pkgs
-rownames(Values) <- Names
-Values["Version",] <- c("0.5", rep("2.3", 8))
-changeDescription(startDir = "C:/rtest/distr/",names=Names,
-                  pkgs=Pkgs, values=Values)
-
-### for 2.4
-Pkgs <- c("SweaveListingUtils", "distr", "distrEx", "distrDoc",
-                      "distrMod", "distrTeach", "distrSim", "distrTEst",
-                      "distrEllipse")
 Names <- c("Version",  "Date", "SVNRevision")
 Values <- matrix(c("2.0.2", format(Sys.time(), format="%Y-%m-%d"),1),3,length(Pkgs))
 colnames(Values) <- Pkgs
 rownames(Values) <- Names
 Values["Version",] <- c("0.6", rep("2.4", 8))
 changeDescription(startDir = "C:/rtest/distr/branches/distr-2.4",names=Names,
+                  pkgs=Pkgs, values=Values)
+
+### for 2.4
+Pkgs <- c("distr", "distrEx", "distrDoc",
+                      "distrMod", "distrTeach", "distrSim", "distrTEst",
+                      "distrEllipse")
+Names <- c("Version",  "Date", "SVNRevision")
+Values <- matrix(c("2.0.2", format(Sys.time(), format="%Y-%m-%d"),1),3,length(Pkgs))
+colnames(Values) <- Pkgs
+rownames(Values) <- Names
+Values["Version",] <- rep("2.5", 8)
+changeDescription(startDir = "C:/rtest/distr/branches/distr-2.5",names=Names,
                   pkgs=Pkgs, values=Values)
 
 #### robast:
