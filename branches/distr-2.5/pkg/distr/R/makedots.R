e647fb119b51c9a547afe32e18c4e591c2e553e9
ruckdeschel <ruckdeschel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-01-11 01:17:36 +0000

    unified dots-manipulations
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/distr@853 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ branches/distr-2.5/pkg/distr/R/makedots.R
@@ -0,0 +1,17 @@
+.makedotsLowLevel <- function(dots){
+       dots$sub <- dots$xlab <- dots$ylab <- dots$main <- dots$type <- NULL
+       dots$xlim <- dots$ylim <- dots$yaxt <- dots$axes <- dots$xaxt <- NULL
+       dots$panel.last <- dots$panel.first <- dots$frame.plot <- dots$ann <-NULL
+       dots$log <- dots$bg <- dots$asp <- NULL
+       return(dots)
+}
+.makedotsL <- function(dots){
+    dots <- .makedotsLowLevel(dots)
+    dots$pch <- dots$cex <- NULL
+    dots
+}
+.makedotsP <- function(dots){
+    dots <- .makedotsLowLevel(dots)
+    dots$lwd <- dots$lty <- NULL
+    dots
+}
