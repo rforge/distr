f04d995589c76fcff6ebd6c0eb9c2d33ce42bd54
kroisand <kroisand@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-09-13 09:58:41 +0000

    check routine adapted such that removing the old version of a library and installing the new happens only if the check succeeded
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/distr@922 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/utils/Rpkgcheck.sh
+++ pkg/utils/Rpkgcheck.sh
@@ -4,16 +4,16 @@ export _R_CHECK_CODOC_S4_METHODS_=true
 export _R_CHECK_SUGGESTS_ONLY_=true
 #
 echo ---------------------------------------------
-echo start full package check
+echo start full package check of $2
 echo ---------------------------------------------
 #
 echo ---------------------------------------------
-echo remove old tar.gz
+echo remove old tar.gz of $2
 echo ---------------------------------------------
 rm -f $2*.tar.gz
 #
 echo ---------------------------------------------
-echo build
+echo build $2
 echo ---------------------------------------------
 $1 CMD build --compact-vignettes="gs+qpdf" --resave-data $2
 #
@@ -23,22 +23,34 @@ echo tar.gz is $tarGz
 echo ---------------------------------------------
 #
 echo ---------------------------------------------
-echo check as cran
+echo check as cran $tarGz
 echo ---------------------------------------------
 $1 CMD check --as-cran --timings $tarGz
+result=$?
 #
 echo ---------------------------------------------
-echo REMOVE
+echo we test that the check of $2 was successful
+echo   i.e. whether the value of check,
+echo     which is $result, is 0
+echo   before removing the old version
+echo   and installing the new version
 echo ---------------------------------------------
-$1 CMD REMOVE $2
-#
-echo ---------------------------------------------
-echo INSTALL
-echo ---------------------------------------------
-$1 CMD INSTALL \
-  --byte-compile --with-keep.source --compact-docs \
-  --resave-data --install-tests --example \
-  --html --latex --clean --preclean \
-  --compile-both \
-  $tarGz
+zero=0
+if [ $result -eq $zero ]; then
+  #
+  echo ---------------------------------------------
+  echo REMOVE $2
+  echo ---------------------------------------------
+  $1 CMD REMOVE $2
+  #
+  echo ---------------------------------------------
+  echo INSTALL $2
+  echo ---------------------------------------------
+  $1 CMD INSTALL \
+    --byte-compile --with-keep.source --compact-docs \
+    --resave-data --install-tests --example \
+    --html --latex --clean --preclean \
+    --compile-both \
+    $tarGz
+fi
 
