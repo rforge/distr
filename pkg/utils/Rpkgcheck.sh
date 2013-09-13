e3348ffcb87d70e4ada0bc4cfaface2fb833f7a0
kroisand <kroisand@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-09-13 09:00:26 +0000

    Rpkgcheck.sh daran angepasst, dass tar.gz genommen werden muss; auch Reihenfolge vertauscht, d.h. zuerst bauen, dann testen und danach erst entfernen und installieren
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/distr@921 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/utils/Rpkgcheck.sh
+++ pkg/utils/Rpkgcheck.sh
@@ -8,9 +8,24 @@ echo start full package check
 echo ---------------------------------------------
 #
 echo ---------------------------------------------
+echo remove old tar.gz
+echo ---------------------------------------------
+rm -f $2*.tar.gz
+#
+echo ---------------------------------------------
 echo build
 echo ---------------------------------------------
-$1 CMD build --compact-vignettes $2
+$1 CMD build --compact-vignettes="gs+qpdf" --resave-data $2
+#
+tarGz=`ls $2*.tar.gz`
+echo ---------------------------------------------
+echo tar.gz is $tarGz
+echo ---------------------------------------------
+#
+echo ---------------------------------------------
+echo check as cran
+echo ---------------------------------------------
+$1 CMD check --as-cran --timings $tarGz
 #
 echo ---------------------------------------------
 echo REMOVE
@@ -20,9 +35,10 @@ $1 CMD REMOVE $2
 echo ---------------------------------------------
 echo INSTALL
 echo ---------------------------------------------
-$1 CMD INSTALL $2
-#
-echo ---------------------------------------------
-echo check
-echo ---------------------------------------------
-$1 CMD check --as-cran --timings $2
+$1 CMD INSTALL \
+  --byte-compile --with-keep.source --compact-docs \
+  --resave-data --install-tests --example \
+  --html --latex --clean --preclean \
+  --compile-both \
+  $tarGz
+
