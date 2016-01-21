4475598ed1534f5302a28610788208f227cdd0c1
ruckdeschel <ruckdeschel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-01-21 11:52:57 +0000

    distr:: new Methods p.r and q.l, synonyma for p and q (useful for use in RStudio)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/distr@1085 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/distr/R/Distribution.R
+++ pkg/distr/R/Distribution.R
@@ -10,9 +10,12 @@ setMethod("param", "Distribution", function(object) object@param)
 setMethod("r", "Distribution", function(object) object@r)
 setMethod("d", "Distribution", function(object) object@d)
 setMethod("p", "Distribution", function(object) object@p)
+setMethod("p.r", "Distribution", function(object) object@p)
 setMethod("q", "Distribution", 
            function(save = "default", status = 0, runLast = TRUE) save@q)
            ### odd arg-list due to existing function in base package 
+setMethod("q.l", "Distribution", 
+           function(object) object@q)
 
 setMethod("p", "Distribution", function(object) object@p)
 setMethod(".lowerExact", "Distribution", function(object){ 
